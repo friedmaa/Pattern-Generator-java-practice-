1afc123db752e2c14afb851b158604292bd9110d
friedmaa <friedmaa@southwestern.edu>
2020-04-23 11:06:36 -0500

    Getting it created in eclipse

---
+++ Pattern_Generator/src/module-info.java
@@ -0,0 +1,2 @@
+module pattern_Generator {
+}
---
+++ Pattern_Generator/src/pattern_generator/PatternGenerator.java
@@ -0,0 +1,11 @@
+package pattern_generator;
+import java.util.*;
+
+public class PatternGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
