8d531530c58f9ef4da5f186f7925e8cd81b80e49
friedmaa <friedmaa@southwestern.edu>
2020-08-26 15:45:44 -0500

    adding random row array method

--- Pattern_Generator/src/pattern_generator/PatternGenerator.java
+++ Pattern_Generator/src/pattern_generator/PatternGenerator.java
@@ -5,7 +5,22 @@ public class PatternGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//System.out.println("test");
+		Scanner console = new Scanner(System.in);
+		createRowNumbers(10,25,5);
 
+	}
+	
+	public static int[] createRowNumbers(int min, int max, int numRows) {
+		int[] rowNumbers = new int[numRows];
+		int newNum = 0;
+		Random random = new Random();
+		for(int i = 0; i < numRows; i++) {
+			newNum = (random.nextInt(max-min) + min);
+			rowNumbers[i] = newNum;
+			System.out.println(rowNumbers[i]);
+		}		
+		return rowNumbers;
 	}
 
 }
