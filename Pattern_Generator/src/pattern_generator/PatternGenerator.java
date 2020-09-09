55f0c6aec5e88869a74592f488988858c13f7179
friedmaa <friedmaa@southwestern.edu>
2020-09-09 13:09:35 -0500

    Generic colors works
    need to remove methods

--- Pattern_Generator/src/pattern_generator/PatternGenerator.java
+++ Pattern_Generator/src/pattern_generator/PatternGenerator.java
@@ -7,30 +7,55 @@ public class PatternGenerator {
 		// TODO Auto-generated method stub
 		//System.out.println("test");
 		int numberOfColors = 0;
+		List<String> listOfColors = new ArrayList<String>();
+		listOfColors = selectColors();
+//		Scanner console = new Scanner(System.in);
+//		System.out.println("Would you like to enter specific colors? (y/n)");
+//		String input = console.next();
+//		if (input.equals("n")) {
+//			createGenericColorsList();
+//		}else if(input.equals("y")){
+//			System.out.println("go to createSpecificColorsList");
+//		} else if(!(input.equals("n"))||!(input.equals("y"))) {
+//			System.out.println("Please enter y or n");
+//		}
+	}
+	//createRowNumbers(10,25,5);
+	//int stripeNumber = howManyStripes();
+	//createGenericColorsList(7);
+
+	public static List<String> selectColors() {
+		List<String> listToReturn = new ArrayList<String>();
 		Scanner console = new Scanner(System.in);
 		System.out.println("Would you like to enter specific colors? (y/n)");
-		if (console.next().equals("n")) {
-			System.out.println("Enter the number of colors you would like to use and they will be labeled as 'Color X'.");
+		String input = console.next();
+		if (input.equals("n")) {
+			//GENERIC Colors
+			int numColors = 0;
 			Scanner colorNumIn = new Scanner(System.in);
+			System.out.println("Enter the number of colors you would like to use and they will be labeled as 'Color X'.");
 			if(!colorNumIn.hasNextInt()){
 				System.out.println("Please enter an integer number");
 				if(colorNumIn.nextInt() <= 0){
 					System.out.println("Please enter a positive integer number");				
 				}
 			}else
-				numberOfColors = colorNumIn.nextInt();
-			createGenericColorsList(numberOfColors);
+				numColors = colorNumIn.nextInt();
+			//createGenericColorsList(numberOfColors);
+			for (int i = 0; i < numColors; i++) {
+				listToReturn.add("Color " +(i+1));//need the i+1 because i starts at 0
+				System.out.println(listToReturn.get(i));
+			}
+		}if(input.equals("y")){
+			//SPECIFIC Colors
+			System.out.println("Please type the name of your color and hit enter, when you are finished, type \"done\"");
+			//System.out.println("go to createSpecificColorsList");
+		} else if(!(input.equals("n"))||!(input.equals("y"))) { 
+			System.out.println("Please enter y or n");
+			selectColors();
 		}
-		//SPECIFIC COLORS
-		if(console.next().equals("y")) {
-			System.out.println("go to createSpecificColorsList");
-			//GENERIC COLORS
-		}
+		return listToReturn;
 	}
-	//createRowNumbers(10,25,5);
-	//int stripeNumber = howManyStripes();
-	//createGenericColorsList(7);
-
 
 	public static List<String> createSpecificColorsList(int numColors){
 		List<String> listToReturn = new ArrayList<String>();
@@ -38,8 +63,18 @@ public class PatternGenerator {
 		return listToReturn;
 	}
 
-	public static List<String> createGenericColorsList(int numColors){
-		
+	public static List<String> createGenericColorsList(){
+		int numColors = 0;
+		Scanner colorNumIn = new Scanner(System.in);
+		System.out.println("Enter the number of colors you would like to use and they will be labeled as 'Color X'.");
+		if(!colorNumIn.hasNextInt()){
+			System.out.println("Please enter an integer number");
+			if(colorNumIn.nextInt() <= 0){
+				System.out.println("Please enter a positive integer number");				
+			}
+		}else
+			numColors = colorNumIn.nextInt();
+		//createGenericColorsList(numberOfColors);
 		List<String> listToReturn = new ArrayList<String>();
 		for (int i = 0; i < numColors; i++) {
 			listToReturn.add("Color " +(i+1));//need the i+1 because i starts at 0
